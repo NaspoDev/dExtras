7d82770847a671a85033e346846db7babeb1fea7
Naspo <a.topaltsis@gmail.com>
2022-10-15 20:59:49 -0400

    Added PackPrompt Feature
    First rendition of the PackPrompt feature. A more free way to enable a server resource pack with improved download prompting.

---
+++ src/main/java/me/dextras/dextras/features/packprompt/PackPrompt.java
@@ -0,0 +1,24 @@
+package me.dextras.dextras.features.packprompt;
+
+import me.dextras.dextras.core.DExtras;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+// Prompts players to download the texture pack listed in the config.
+// Does not require a server resource pack to be set in the server.properties (allows more freedom).
+public class PackPrompt implements Listener {
+
+    private DExtras plugin;
+    public PackPrompt(DExtras plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    private void joinEvent(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.setResourcePack(plugin.getConfig().getString("pack-prompt.pack-url"),
+                null, plugin.getServer().getResourcePackPrompt());
+    }
+}
