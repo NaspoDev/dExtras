74cf44a0109901cde3dfff7b0485696f9b069599
Naspo <a.topaltsis@gmail.com>
2022-10-15 21:12:00 -0400

    Bug fixes
    Added try catch for setting the resource pack.

--- src/main/java/me/dextras/dextras/features/packprompt/PackPrompt.java
+++ src/main/java/me/dextras/dextras/features/packprompt/PackPrompt.java
@@ -6,6 +6,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.logging.Level;
+
 // Prompts players to download the texture pack listed in the config.
 // Does not require a server resource pack to be set in the server.properties (allows more freedom).
 public class PackPrompt implements Listener {
@@ -18,7 +20,12 @@ public class PackPrompt implements Listener {
     @EventHandler
     private void joinEvent(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        player.setResourcePack(plugin.getConfig().getString("pack-prompt.pack-url"),
-                null, plugin.getServer().getResourcePackPrompt());
+
+        try {
+            player.setResourcePack(plugin.getConfig().getString("pack-prompt.pack-url"),
+                    null, plugin.getServer().getResourcePackPrompt());
+        } catch (Exception e) {
+            plugin.getLogger().log(Level.WARNING, "Could not load resource pack!");
+        }
     }
 }
