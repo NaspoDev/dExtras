0d1e98d28f020441b27c451509df80734339e49c
Naspo <a.topaltsis@gmail.com>
2023-04-05 13:22:27 -0400

    Implimented Netherite Rank
    Netherite rank was reverted to a permanent lifetime rank, hence it's inclusion can now be properly implimented.

--- src/main/java/me/dextras/dextras/features/restoreclaimblocks/RestoreClaimBlocks.java
+++ src/main/java/me/dextras/dextras/features/restoreclaimblocks/RestoreClaimBlocks.java
@@ -15,11 +15,6 @@ import java.util.List;
 /*
 Restore Claim Blocks Feature:
 Automatically restores claim blocks of ranked players on first join (upon season turnover).
-
-Does not restore for Netherite rank players!
-This is because Netherite is currently a temporary rank, with each duration of the rank
-having different amount of claim blocks. Therefore making it impossible to tell which duration a player had purchased.
-Instead, the plugin notifies the player to make a support ticket, and a server administrator can restore them manually.
  */
 
 public class RestoreClaimBlocks implements Listener {
@@ -30,7 +25,7 @@ public class RestoreClaimBlocks implements Listener {
     public RestoreClaimBlocks(DExtras plugin) {
         this.plugin = plugin;
 
-        // List of rank objects (DeltaRealms ranks) excluding netherite.
+        // List of rank objects (DeltaRealms ranks).
         // Rank objects have the name, priority, and allocated claim blocks.
         // (Priority is in ascending order. Ex. 2 is higher than 1).
         ranks = new ArrayList<>();
@@ -39,6 +34,7 @@ public class RestoreClaimBlocks implements Listener {
         ranks.add(new Rank("gold", 3, 3500));
         ranks.add(new Rank("diamond", 4, 6000));
         ranks.add(new Rank("emerald", 5, 8000));
+        ranks.add(new Rank("netherite", 6, 10000));
     }
 
     @EventHandler
@@ -48,12 +44,6 @@ public class RestoreClaimBlocks implements Listener {
 
         // If they haven't played before.
         if (!(player.hasPlayedBefore())) {
-
-            // If they have netherite rank, send the custom alert notice and exit.
-            if (player.hasPermission("group.netherite")) {
-                sendNetheriteNotification(player);
-                return;
-            }
 
             // If they have a rank, store it. (Stores the highest priority rank if they have multiple).
             for (Rank rank : ranks) {
@@ -91,19 +81,6 @@ public class RestoreClaimBlocks implements Listener {
         // Send a message and play a sound to notify player that claim blocks have been restored.
         player.sendMessage(Utils.chatColor("&a" + claimBlocks + "&aclaim blocks for your rank have been restored."));
         player.playSound(player.getLocation(), Sound.BLOCK_AMETHYST_BLOCK_CHIME, 0.5f, 1.0f);
-    }
-
-    // Send Netherite rank players a message to create a ticket on discord to restore claim blocks.
-    private void sendNetheriteNotification(Player player) {
-        // After 30 seconds...
-        Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
-            @Override
-            public void run() {
-                player.sendMessage("&eClaim blocks for those with Netherite rank cannot restore automatically. " +
-                        "&ePlease create a ticket on our discord to restore them!");
-                player.playSound(player.getLocation(), Sound.BLOCK_AMETHYST_BLOCK_CHIME, 0.5f, 1.0f);
-            }
-        }, 1200L);
     }
 
 }
